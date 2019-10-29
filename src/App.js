0fb4ff593024c1861c20f04478d48b263629b429
Christian-Ford <fjord.christian@gmail.com>
2019-10-28 23:13:20 -0500

    Started work on Call To Action

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 
 import Navbar from './components/Navbar.jsx';
+import CallToAction from './components/CallToAction.jsx';
+
 import './css/index.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <CallToAction />
     </div>
   );
 }
---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const CallToAction = () => {
+    return (
+        <div className="ctaWrap">
+            <div className="brand">
+                <h1>CHRISTIAN FORD</h1>
+            </div>
+
+            <div>
+              <p className="context">  Web Developer  | UI Designer  | App Developer </p>
+            </div>
+        </div>
+    )
+}
+
+export default CallToAction;
