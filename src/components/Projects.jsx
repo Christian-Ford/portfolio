ee65948f5af2de164110a2c80a480b6c0e2e4b3a
Christian-Ford <fjord.christian@gmail.com>
2019-10-31 14:47:54 -0500

    Added Project objects

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -2,7 +2,63 @@ import React, { Component } from 'react';
 
 import ProjectCard from './projects/ProjectCard.jsx';
 
+const projectsList = [
+    {
+        name: 'project 1' ,
+        pic: 'img/blank/blank' ,
+        description: 'lorem ipsum' ,
+        github: 'www.github.com' ,
+        trello: 'www.trello.com' ,
+        liveLink: 'www.broken.com'
 
+    } ,
+    {
+        name: 'project 2' ,
+        pic: 'img/blank/blank' ,
+        description: 'lorem ipsum' ,
+        github: 'www.github.com' ,
+        trello: 'www.trello.com' ,
+        liveLink: 'www.broken.com'
+
+    } ,
+    {
+        name: 'project 3' ,
+        pic: 'img/blank/blank' ,
+        description: 'lorem ipsum' ,
+        github: 'www.github.com' ,
+        trello: 'www.trello.com' ,
+        liveLink: 'www.broken.com'
+
+    } ,
+    {
+        name: 'project 1' ,
+        pic: 'img/blank/blank' ,
+        description: 'lorem ipsum' ,
+        github: 'www.github.com' ,
+        trello: 'www.trello.com' ,
+        liveLink: 'www.broken.com'
+
+    } ,
+    {
+        name: 'project 1' ,
+        pic: 'img/blank/blank' ,
+        description: 'lorem ipsum' ,
+        github: 'www.github.com' ,
+        trello: 'www.trello.com' ,
+        liveLink: 'www.broken.com'
+
+    } ,
+    {
+        name: 'project 1' ,
+        pic: 'img/blank/blank' ,
+        description: 'lorem ipsum' ,
+        github: 'www.github.com' ,
+        trello: 'www.trello.com' ,
+        liveLink: 'www.broken.com'
+
+    } ,
+   
+]
 
 export default class Projects extends Component {
     render() {
